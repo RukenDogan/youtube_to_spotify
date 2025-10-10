0ba71de84e28ee5a871ab79c2170e66c0b385b84
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-10-10 23:31:15 +0200

    tests unitaires flask et classe main.py

---
+++ backend/tests/test_flask.py
@@ -0,0 +1,17 @@
+from app import app
+
+def test_youtube_to_spotify():
+    client = app.test_client()
+    
+    response = client.post(
+        "/youtube-to-spotify",
+        json={"youtube_url": "https://www.youtube.com/playlist?list=PL0C00MH_AB4eundtu0PqswbWyYHdZawZi"}
+    )
+    
+    # vérifier que la requête a réussi
+    assert response.status_code == 200
+    
+    # vérifier que la réponse contient bien 'playlist_name'
+    data = response.get_json()
+    assert "playlist_name" in data
+    assert "nb_tracks_added" in data
---
+++ backend/tests/test_synchronizer.py
@@ -0,0 +1,11 @@
+from main import Synchronizer
+from backend.models.spotify_model import SPOTIFY_USER_ID
+
+def test_synchronizer():
+    youtube_url = "https://www.youtube.com/playlist?list=PL0C00MH_AB4eundtu0PqswbWyYHdZawZi"
+    sync = Synchronizer(youtube_url, SPOTIFY_USER_ID)
+    sync.extract_youtube()
+    sync.create_spotify_playlist()
+    sync.add_tracks_to_spotify()
+
+    assert len(sync.track_ids) > 0
