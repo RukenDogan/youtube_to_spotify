b4113de5c3e58e00c490747edee84d1fba5792d9
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-10-20 11:18:59 +0200

    ajout loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import "./styles/App.css";
+import Loader from "./components/Loader.jsx";
 
 
 export default function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = (userData) => {
+    setLoading(true);
+    setTimeout(() => {
+      setUser(userData);
+      setLoading(false);
+    }, 1500);
+  };
 
   return (
     <div className="appContainer">
-      {user ? <Dashboard /> : <Login onLogin={setUser} />}
+      {loading && <Loader />}
+      {user ? <Dashboard /> : <Login onLogin={handleLogin} />}
     </div>
   );
 }
--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./Loader.css";
+import "../styles/Loader.css";
 
 export default function Loader() {
   return (
     <div className="loader">
       <div className="loader-inner">
-        <div className="loader-item"></div>
         <div className="loader-item"></div>
         <div className="loader-item"></div>
         <div className="loader-item"></div>
