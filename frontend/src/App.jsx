20193a1bfc9d74a8308d14b2e4a59e3227127a10
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-10-31 23:42:04 +0100

    test résolution route front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,47 @@
-// App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import "./styles/App.css";
+import Loader from "./components/Loader.jsx";
+import Footer from "./components/Footer.jsx";
+
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = (userData) => {
+    setLoading(true);
+    setTimeout(() => {
+      setUser(userData);
+      setLoading(false);
+    }, 1500);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="appContainer">
+      <div className="content">
+        {loading && <Loader loading={loading} />}
+        {!loading && (user ? <Dashboard /> : <Login onLogin={handleLogin} />)}
+      </div>
+      <Footer />
+    </div>
   );
 }
 
 
-// import { useState } from "react";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-// import "./styles/App.css";
-// import Loader from "./components/Loader.jsx";
-// import Footer from "./components/Footer.jsx";
-
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login.jsx";
+// import Dashboard from "./pages/Dashboard.jsx";
 
 // export default function App() {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(false);
-
-//   const handleLogin = (userData) => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       setUser(userData);
-//       setLoading(false);
-//     }, 1500);
-//   };
-
 //   return (
-//     <div className="appContainer">
-//       <div className="content">
-//         {loading && <Loader loading={loading} />}
-//         {!loading && (user ? <Dashboard /> : <Login onLogin={handleLogin} />)}
-//       </div>
-//       <Footer />
-//     </div>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </BrowserRouter>
 //   );
 // }
+
