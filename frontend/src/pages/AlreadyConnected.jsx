6ae64d53ee2f3f859b28dee5089a3beaeb02494e
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-11-08 20:10:00 +0100

    ajout du loader page de redirection + page notfound

--- frontend/src/pages/AlreadyConnected.jsx
+++ frontend/src/pages/AlreadyConnected.jsx
@@ -1,7 +1,8 @@
 import "../styles/AlreadyConnected.css";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import spotifyLogo from "../assets/images/logoBS.png"; // ton logo BeatSync ou Spotify
+import spotifyLogo from "../assets/images/logoBS.png";
+import Loader from "../components/Loader.jsx";
 
 export default function AlreadyConnected() {
   const navigate = useNavigate();
@@ -9,7 +10,7 @@ export default function AlreadyConnected() {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/dashboard");
-    }, 2500); // 2,5 secondes
+    }, 3000); // 3,0 secondes
 
     return () => clearTimeout(timer);
   }, [navigate]);
@@ -19,7 +20,8 @@ export default function AlreadyConnected() {
       <img src={spotifyLogo} alt="Logo" className="alreadyConnectedLogo" />
       <h2 className="alreadyConnectedTitle">Vous êtes déjà connecté à Spotify !</h2>
       <p className="alreadyConnectedText">Redirection vers le dashboard...</p>
-      <div className="spinner"></div>
+      <Loader loading={true} />
+
     </div>
   );
 }
